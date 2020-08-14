f9e9c78b9552164c41523b5ebf2fe3b54e0cd7fd
Beatka9 <beatricepinciaroli9@gmail.com>
2020-08-14 16:35:42 +0200

    Fix unit conversion

--- src/TemperatureNow.js
+++ src/TemperatureNow.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TemperatureNow.css";
 
 export default function TemperatureNow(props) {
-  let temperature = props.celsius;
+  let celsiusTemperature = props.celsius;
+  let farenheitTemperature = (props.celsius * 9) / 5 + 32;
+  let [units, setUnits] = useState("celsius");
 
-  function farTemperature(event) {
+  function setFarenheit(event) {
     event.preventDefault();
-    temperature = (props.celsius * 9) / 5 + 32;
+    setUnits("farenheit");
   }
-  function celsiusTemp(event) {
+  function setCelsius(event) {
     event.preventDefault();
-    temperature = props.celsius;
+    setUnits("celsius");
   }
-  return (
-    <p>
-      <span className="TemperatureNow">{Math.round(temperature)} </span>
-      <span className="streched-link" onClick={celsiusTemp}>
-        C°
-      </span>
-      <span className="streched-link" onClick={farTemperature}>
-        |F°
-      </span>
-    </p>
-  );
+
+  if (units === "celsius") {
+    return (
+      <p>
+        <span className="TemperatureNow">
+          {Math.round(celsiusTemperature)}{" "}
+        </span>
+        <span className="streched-link">C°</span>
+        <span className="streched-link" onClick={setFarenheit}>
+          |F°
+        </span>
+      </p>
+    );
+  } else {
+    return (
+      <p>
+        <span className="TemperatureNow">
+          {Math.round(farenheitTemperature)}
+        </span>
+        <span className="streched-link" onClick={setCelsius}>
+          C°
+        </span>
+        <span className="streched-link">|F°</span>
+      </p>
+    );
+  }
 }
